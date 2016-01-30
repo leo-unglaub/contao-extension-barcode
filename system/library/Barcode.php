1b09e9c450ae3e513480ab09fd99b1f68c1ab551
Leo Unglaub <leo@leo-unglaub.net>
2016-01-30 19:26:34 +0100

    Adding a shorter copyright header
    Signed-off-by: Leo Unglaub <leo@leo-unglaub.net>

--- system/library/Barcode.php
+++ system/library/Barcode.php
@@ -1,30 +1,12 @@
 <?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2010 Leo Feyer
+ * contao-barcode
  *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Leo Unglaub 2013
- * @author     Leo Unglaub <leo@leo-unglaub.net>
- * @package    barcode
- * @license    LGPL
+ * @copyright	Leo Unglaub 2016
+ * @author		Leo Unglaub <leo@leo-unglaub.net>
+ * @package		contao-barcode
+ * @license		GPL
  */
 
 
